6148c1237accad08b8ea4307d8516007c8d28266
Bela-Szilagyi <szilagyib.mail@gmal.com>
2017-06-10 00:31:46 +0200

    randomized list

--- MatchColorJS.js
+++ MatchColorJS.js
@@ -8,11 +8,13 @@ function main() {
 function drawTable(size) {
     size = $('#' + size).val();
     table = '<table border="solid">';
+    shuffledItems = randomize((Math.pow(2, size)/2));
+    // alert('shuffledItems: ' + shuffledItems);
     var index = 0;
     for (var row = 0; row < size; row++ ) {
         table += '<tr>';
             for (var column = 0; column < size; column++) {
-                table += '<td class="cell" id="' + index + '">' + index + '</td>';
+                table += '<td class="cell" id="' + index + '">' + shuffledItems[index] + '</td>';
                 index++;
             } 
         table += '</tr>';
@@ -26,4 +28,36 @@ function drawTable(size) {
 
 function handleClickedCell(cellId) {
             alert(cellId);    
+}
+
+function randomize(sizeOfArray) {
+    listToRandomize = getOrderedLIst(sizeOfArray);
+
+    /* Return a random number between 1 and 10:
+       Math.floor((Math.random() * 10) + 1); */
+    
+    var randomizedList = [];
+    // alert('listToRandomize: ' + listToRandomize);
+    numberOfIterations = listToRandomize.length;
+    for(i=0; i < numberOfIterations; i++) {
+        // alert('iteration: ' + i);
+        randomNumber = Math.floor((Math.random() * (listToRandomize.length)));
+        // alert('randomNumber: ' + randomNumber);
+        randomizedList.push(listToRandomize[randomNumber]);
+        // alert('randomizedList: ' + randomizedList);
+        listToRandomize.splice(randomNumber, 1);
+        // alert('listToRandomize: ' + listToRandomize); 
+}
+    return randomizedList;
+}
+
+function getOrderedLIst(listSize) {
+    result = [];
+    for (i = 0; i < listSize; i++) {
+        result.push(i);
+    }
+    for (i = 0; i < listSize; i++) {
+        result.push(i);
+    }
+    return result;
 }
