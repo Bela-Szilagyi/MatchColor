f048f692387bcc8fb59291fb74e8a1fbdee2d701
Bela-Szilagyi <szilagyib.mail@gmal.com>
2017-06-09 20:11:28 +0200

    clickable cells

--- MatchColorJS.js
+++ MatchColorJS.js
@@ -1,22 +1,29 @@
 
 function main() {
+    $('.cell').on('click', function() {
+        handleClickedCell(this.id);
+    });
 }
 
 function drawTable(size) {
     size = $('#' + size).val();
-    table = '<table>';
+    table = '<table border="solid">';
+    var index = 0;
     for (var row = 0; row < size; row++ ) {
         table += '<tr>';
             for (var column = 0; column < size; column++) {
-                table += '<td>' + '0' + '</td>';
+                table += '<td class="cell" id="' + index + '">' + index + '</td>';
+                index++;
             } 
         table += '</tr>';
     }
     table += '</table>'
     var newTable = $('#table')[0];
-    alert(newTable + table);
     newTable.innerHTML = '' ;
     newTable.innerHTML += table;
+    main();
 }
 
-$(document).ready(main);
+function handleClickedCell(cellId) {
+            alert(cellId);    
+}
