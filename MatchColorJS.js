97b3d2e6096fd0ba2fc36cd0d73cf82a796c3892
Bela-Szilagyi <szilagyib.mail@gmal.com>
2017-06-09 14:40:16 +0200

    first attempt of drawing table

--- MatchColorJS.js
+++ MatchColorJS.js
@@ -2,9 +2,23 @@
 function main() {
 }
 
-function setTableSize() {
+function drawTable(size) {
+    // size=getTableSize();
+    table = '';
+    for (var row = 0; row < size; row++ ) {
+        table += '<tr>';
+            for (var column = 0; column < size; column++) {
+                table += '<td>' + '0' + '</td>';
+            } 
+        table += '</tr>';
+    }
+    document.write('<table>' + table + '</table>');
+}
+
+function getTableSize() {
         var tableSize = $('#tableSize').val();
         alert(tableSize);
+        drawTable(tableSize);
 }
 
 $(document).ready(main);
